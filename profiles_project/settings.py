73c848bdf36c8e9e198b06b8f3722ec178b24d38
samrat <samrat.b.shah2016@gmail.com>
2022-01-28 20:43:24 +0530

    adding deploy folder

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'e9fpd3bye-+_+&p8u*anrh%b!(b1u164h0#8_vve8oc$sy9z-_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG',1)))
 
 ALLOWED_HOSTS = ['ec2-34-201-54-20.compute-1.amazonaws.com','127.0.0.1']
 
@@ -123,3 +123,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'profiles_api.UserProfile'
+
+STATIC_ROOT = 'static/'
